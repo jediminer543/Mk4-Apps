1a5ea386693a384a4e6aaaeac955dc738b6aedad
jediminer543 <jediminer543@users.noreply.github.com>
2018-09-02 08:43:47 +0100

    Created cheap hacky irc client
    PLS WORK

---
+++ irc/main.py
@@ -0,0 +1,76 @@
+"""IRC app for reading and sending messages
+"""
+___name___         = "IRC"
+___license___      = "WTFPL"
+___dependencies___ = ["app", "dialogs", "ugfx_helper", "buttons"]
+___categories___   = ["phone"]
+___bootstrapped___ = True
+
+
+from app import *
+from dialogs import *
+import ugfx
+import ugfx_helper
+import socket
+import buttons
+from tilda import Buttons
+
+ugfx_helper.init()
+ugfx.clear()
+
+
+wifi.connect(show_wait_message=True)
+
+nick = prompt_text("Nickname (enter or add number)", init_text="emfbadge")
+
+ugfx.clear()
+
+notice("Connecting...")
+
+remoteaddr = "chat.freenode.net"
+remoteport = 6697
+
+ircchan = "#emfcamp"
+
+addr = socket.getaddrinfo("chat.freenode.com", 6667)
+
+conn = socket.connect()
+conn.connect(addr)
+
+conn.send("PASS seecretpassword\r\n")
+conn.send("NICK {}\r\n".format(nick))
+conn.send("USER {} 0 * :An EMF camp Badge\r\n".format(nick))
+
+conn.send("JOIN {}\r\n".format(ircchan))
+
+conn.setTimeout(None)
+
+ugfx.clear()
+notice("Connected!")
+
+while True:
+    data = ""
+    try:
+      data = conn.readline()
+    except Exception as e:
+      # TODO: Notify of exception
+      notify("ERROR")
+      app.restart_to_default()
+    if not data == None and not data == "":
+      if data.split(" ", 1)[1].startswith("PRIVMSG"):
+        unameEOS = data.indexof("!")
+        username = data[1:unameEOS]
+        mesg = data.split(":", 2)[2]
+        message = (uname, mesg)
+        ugfx.clear()
+        notify("{}:\r\n{}".format(message))
+      elif data.split(" ", 1)[1].startswith("PING"):
+        conn.send("PONG\r\n")
+    if Buttons.is_pressed(Buttons.BTN_A):
+      ugfx.clear()
+      mesg = prompt_text("Message to Send", init_text="")
+      if not mesg == "":
+        conn.send("PRIVMSG {} :{}\r\n".format(ircchan, mesg))
+    elif Buttons.is_pressed(Buttons.BTN_B):
+      app.restart_to_default()
+    sleep(1)
